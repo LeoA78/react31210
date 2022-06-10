cc26341d617104cc8f6e3efa35c95bdc12fa139e
Leonel Altamirano <altamiranopedroleonel@outlook.com>
2022-06-09 21:23:33 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://coderhouse.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendiendo React en CoderHouse
         </a>
       </header>
     </div>
