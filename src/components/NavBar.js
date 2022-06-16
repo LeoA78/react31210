89ad049aad89854a2a66923bf5c0c929188479b9
Leonel Altamirano <altamiranopedroleonel@outlook.com>
2022-06-15 21:22:39 -0300

    Estilos para el header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,36 @@
-import './navbar.css';
-import logo from '../assets/images/logo.png';
+import "./navbar.css";
+import logo from "../assets/images/logo.png";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import MenuIcon from '@mui/icons-material/Menu';
 
 function NavBar() {
   return (
     <>
       <header>
-        <div className='brand'>
+        <div className="brand">
           <img src={logo} alt="Logo de Vibes" />
         </div>
 
-        <nav className='menu'>
-          <ul className='menu-list'>
-            <li className='item'>Mujer</li>
-            <li className='item'>Hombre</li>
-            <li className='item'>Infantil</li>
-            <li className='item'>Deportivo</li>
-            <li className='item'>Ofertas</li>
+        <nav className="menu">
+
+          <input type="checkbox" id="check" />
+
+          <label for="check" class="checkbtn">
+            <i class="menu-icon">
+              <MenuIcon/>
+            </i>
+          </label>
+
+          <ul className="menu-list">
+            <li className="item"><a href="#">Mujer</a></li>
+            <li className="item"><a href="#">Hombre</a></li>
+            <li className="item"><a href="#">Infantil</a></li>
+            <li className="item"><a href="#">Deportivo</a></li>
+            <li className="item"><a href="#">Ofertas</a></li>
           </ul>
+
         </nav>
-
-        <h2>Carrito</h2>
-
+        <ShoppingBagIcon fontSize="large" />
       </header>
     </>
   );
